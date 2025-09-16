0ba3866eb1acbffcab5bea3bca18a0efdfdbc51b
Doru Blânzeanu <dblnz@pm.me>
2025-09-16 22:46:03 +0300

    Update Mason config and format files

--- lua/dblnz/plugins/telescope.lua
+++ lua/dblnz/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
-    -- change some telescope options and a keymap to browse plugin files
-    "nvim-telescope/telescope.nvim",
-    keys = {
+	-- change some telescope options and a keymap to browse plugin files
+	"nvim-telescope/telescope.nvim",
+	keys = {
         -- add a keymap to browse plugin files
         -- stylua: ignore
         {
@@ -10,38 +10,46 @@ return {
             desc = "Find Plugin File",
             mode = { "n" },
         },
-        {
-            "<C-f>g",
-            function() require("telescope.builtin").git_files() end,
-            desc = "Find Git Files",
-            mode = { "n" },
-        },
-        {
-            "<C-f>s",
-            function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end,
-            desc = "Search for String",
-            mode = { "n" },
-        },
-        {
-            "<C-f>w",
-            function() require("telescope.builtin").grep_string({ search = vim.fn.expand('<cword>') }) end,
-            desc = "Search for word under cursor",
-            mode = { "n" },
-        },
-        {
-            "<C-f>W",
-            function() require("telescope.builtin").grep_string({ search = vim.fn.expand('<cWORD>') }) end,
-            desc = "Search for word under cursor including special chars",
-            mode = { "n" },
-        },
-    },
-    -- change some options
-    opts = {
-        defaults = {
-            layout_strategy = "horizontal",
-            layout_config = { prompt_position = "top" },
-            sorting_strategy = "ascending",
-            winblend = 0,
-        },
-    },
+		{
+			"<C-f>g",
+			function()
+				require("telescope.builtin").git_files()
+			end,
+			desc = "Find Git Files",
+			mode = { "n" },
+		},
+		{
+			"<C-f>s",
+			function()
+				require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+			end,
+			desc = "Search for String",
+			mode = { "n" },
+		},
+		{
+			"<C-f>w",
+			function()
+				require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") })
+			end,
+			desc = "Search for word under cursor",
+			mode = { "n" },
+		},
+		{
+			"<C-f>W",
+			function()
+				require("telescope.builtin").grep_string({ search = vim.fn.expand("<cWORD>") })
+			end,
+			desc = "Search for word under cursor including special chars",
+			mode = { "n" },
+		},
+	},
+	-- change some options
+	opts = {
+		defaults = {
+			layout_strategy = "horizontal",
+			layout_config = { prompt_position = "top" },
+			sorting_strategy = "ascending",
+			winblend = 0,
+		},
+	},
 }
