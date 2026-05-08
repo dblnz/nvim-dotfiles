dc633e17dab48fbd4b8b2d02451edbd78fd23339
Doru Blânzeanu <dblnz@pm.me>
2026-05-08 13:06:39 +0300

    Change telescope shortcuts and scroll
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- lua/dblnz/config/options.lua
+++ lua/dblnz/config/options.lua
@@ -26,7 +26,8 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 6
+vim.opt.smoothscroll = true
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
--- lua/dblnz/plugins/telescope.lua
+++ lua/dblnz/plugins/telescope.lua
@@ -10,11 +10,11 @@ return {
 		{ "<leader><leader>", function() require("telescope.builtin").find_files() end, desc = "Find files" },
 		{ "<leader>/", function() require("telescope.builtin").live_grep() end, desc = "Live grep" },
 		-- Custom bindings
-		{ "<C-f>f", function() require("telescope.builtin").find_files() end, desc = "Find files", mode = { "n" } },
-		{ "<C-f>g", function() require("telescope.builtin").git_files() end, desc = "Find git files", mode = { "n" } },
-		{ "<C-f>s", function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end, desc = "Search for string", mode = { "n" } },
-		{ "<C-f>w", function() require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") }) end, desc = "Search word under cursor", mode = { "n" } },
-		{ "<C-f>W", function() require("telescope.builtin").grep_string({ search = vim.fn.expand("<cWORD>") }) end, desc = "Search WORD under cursor", mode = { "n" } },
+		{ "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find files", mode = { "n" } },
+		{ "<leader>fg", function() require("telescope.builtin").git_files() end, desc = "Find git files", mode = { "n" } },
+		{ "<leader>fs", function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end, desc = "Search for string", mode = { "n" } },
+		{ "<leader>fw", function() require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") }) end, desc = "Search word under cursor", mode = { "n" } },
+		{ "<leader>fW", function() require("telescope.builtin").grep_string({ search = vim.fn.expand("<cWORD>") }) end, desc = "Search WORD under cursor", mode = { "n" } },
 		-- Buffers and help
 		{ "<leader>fb", function() require("telescope.builtin").buffers() end, desc = "Find buffers" },
 		{ "<leader>fh", function() require("telescope.builtin").help_tags() end, desc = "Help tags" },
